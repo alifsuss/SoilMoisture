37d9dac3d0971e339d4643fb98ee6e9b605a1cdd
Alif Sussardi <73863478+alifsuss@users.noreply.github.com>
2020-11-04 20:44:06 +0000

    Update main.cpp
    this seems simplistic, but the purpose is to give value such as:  48,25,95   <- value being Humidity,Temp,Moisture --> this will be fed to the RPi3 and processed with Python script to get csv files.

--- main.cpp
+++ main.cpp
@@ -1,52 +1,52 @@
-#include "mbed.h"
-#include "DHT22.h"
+#include "mbed.h"  
+#include "DHT22.h" //this is the moisture sensor 
 
 //------------------------------------
 // Hyperterminal configuration
 // 9600 bauds, 8-bit data, no parity
 //------------------------------------
 
-//Serial pc(SERIAL_TX, SERIAL_RX); //this is to pc
-Serial pc(PA_11, PA_12, 9600); // this from pins to RPi3
+//Serial pc(SERIAL_TX, SERIAL_RX);    //this is to pc if you connect your ST to PC via USB
+Serial pc(PA_11, PA_12, 9600);        // this from pins to RPi3
 
-DigitalOut myled(LED1);
-AnalogIn analogRead(PA_0);
-DHT22 dhtSensor(PA_1);
+DigitalOut myled(LED1);              //this to turn on light
+AnalogIn analogRead(PA_0);           // declare the analog signal which is Air Humidity Sensor
+DHT22 dhtSensor(PA_1);              //This is the DHT22 port
 
 int main()
 {
-    float humidityVal = 0;
+    float humidityVal = 0;         //All to set initial values
     float temperatureVal = 0;
     float moistureVal = 0;
     float moistcalib = 0;
     
     int i = 1;
     while(1) {
-        wait(600);
+        wait(600);                                                  //this the wait time that the sensor needs
         //printf("This program runs since %d seconds.\n\n", i);
         //i++;
-        i = i + 1;
+        i = i + 1;                                                  
         myled = !myled;
         
         //print time
-        //time_t my_time = time(NULL);
+        //time_t my_time = time(NULL);                      // I didnt print time, but use the time from RPi3 instead
         //pc.printf("%s", ctime(&my_time));
                
         
         // DHT22 Humidity
-        dhtSensor.readData();
-        humidityVal = dhtSensor.getHumidity();
+        dhtSensor.readData();                              //calling the sensor
+        humidityVal = dhtSensor.getHumidity();             //recording air humidity.. see DHT22 manual
         //pc.printf("Humidity: %f.\n", humidityVal);
-        pc.printf("%.1f,", humidityVal);
+        pc.printf("%.1f,", humidityVal);                    //printing humidity
         
         //DHT22 temperature
-        temperatureVal = dhtSensor.getTemperatureC();
+        temperatureVal = dhtSensor.getTemperatureC();           //declaring new temperature value
         //pc.printf("Temperature: %f.\n", temperatureVal); 
-        pc.printf("%.1f,", temperatureVal); 
+        pc.printf("%.1f,", temperatureVal);                     //printing temperature
         
         //MHS moisture
-        moistureVal = analogRead;
-        moistcalib = (1 - moistureVal)*100;
-        pc.printf("%.1f", moistcalib); 
+        moistureVal = analogRead;                       //declaring soil moisture
+        moistcalib = (1 - moistureVal)*100;             // moisture was measured in fraction of Voltage detected (Here I use 3.5V port) then multiplied by 100,
+        pc.printf("%.1f", moistcalib);              // declaring the moisture
     }
 }
